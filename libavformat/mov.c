cc75dbd10fa881dc50795c3d2c64171b9f0b304d
Zhao Zhili <zhilizhao@tencent.com>
2020-09-17 17:45:01 +0800

    avformat/mov: replace multiple avio_r8() by avio_skip()

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -103,9 +103,7 @@ static int mov_metadata_int8_bypass_padding(MOVContext *c, AVIOContext *pb,
                                             unsigned len, const char *key)
 {
     /* bypass padding bytes */
-    avio_r8(pb);
-    avio_r8(pb);
-    avio_r8(pb);
+    avio_skip(pb, 3);
 
     c->fc->event_flags |= AVFMT_EVENT_FLAG_METADATA_UPDATED;
     av_dict_set_int(&c->fc->metadata, key, avio_r8(pb), 0);
