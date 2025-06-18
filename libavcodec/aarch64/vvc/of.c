97a1e51b60936c7402267827138e0623bc8613bc
Zhao Zhili <zhilizhao@tencent.com>
2025-06-18 12:15:37 +0800

    tmp4

--- libavcodec/aarch64/vvc/of.c
+++ libavcodec/aarch64/vvc/of.c
@@ -37,27 +37,7 @@ void ff_vvc_prof_grad_filter_8x_neon(int16_t *gradient_h,
 
 #define VVC_SIGN(v) (v < 0 ? -1 : !!v)
 
-static void vvc_derive_bdof_vx_vy_8x(const int16_t *_src0, const int16_t *_src1, int16_t *gradient_h[2], int16_t *gradient_v[2], int16_t vx[16], int16_t vy[16], int block_h)
-{
-    int line_table[2][4][5] = {0};
-    int block_w = 8;
-    for (int y = 0; y < block_h; y += BDOF_MIN_BLOCK_SIZE) {
-        for (int x = 0; x < block_w; x += BDOF_MIN_BLOCK_SIZE) {
-            const int16_t *src0 = _src0 + y * MAX_PB_SIZE + x;
-            const int16_t *src1 = _src1 + y * MAX_PB_SIZE + x;
-            int idx = BDOF_BLOCK_SIZE * y + x;
-            const int16_t *gh[] = {gradient_h[0] + idx, gradient_h[1] + idx};
-            const int16_t *gv[] = {gradient_v[0] + idx, gradient_v[1] + idx};
-            const int shift2 = 4;
-            const int shift3 = 1;
-            const int thres = 1 << 4;
-            int sgx2 = 0, sgy2 = 0, sgxgy = 0, sgxdi = 0, sgydi = 0;
-            const int padLeft = !x;
-            const int padTop = !y;
-            const int padRight = x + BDOF_MIN_BLOCK_SIZE == block_w;
-            const int padBottom = y + BDOF_MIN_BLOCK_SIZE == block_h;
-
-            for (int y2 = -1; y2 < BDOF_MIN_BLOCK_SIZE + 1; y2++) {
+/*
                 if (y == 0) {
                     // pad top, copy last line
                     if (y2 == 0) {
@@ -96,6 +76,46 @@ static void vvc_derive_bdof_vx_vy_8x(const int16_t *_src0, const int16_t *_src1,
                     }
                 }
 
+                if (y2 == BDOF_MIN_BLOCK_SIZE - 1 || (y == 0 && y2 == -1)) {
+                    line_table[0][x / BDOF_MIN_BLOCK_SIZE][0] = save[0];
+                    line_table[0][x / BDOF_MIN_BLOCK_SIZE][1] = save[1];
+                    line_table[0][x / BDOF_MIN_BLOCK_SIZE][2] = save[2];
+                    line_table[0][x / BDOF_MIN_BLOCK_SIZE][3] = save[3];
+                    line_table[0][x / BDOF_MIN_BLOCK_SIZE][4] = save[4];
+                } else if (y2 == BDOF_MIN_BLOCK_SIZE) {
+                    line_table[1][x / BDOF_MIN_BLOCK_SIZE][0] = save[0];
+                    line_table[1][x / BDOF_MIN_BLOCK_SIZE][1] = save[1];
+                    line_table[1][x / BDOF_MIN_BLOCK_SIZE][2] = save[2];
+                    line_table[1][x / BDOF_MIN_BLOCK_SIZE][3] = save[3];
+                    line_table[1][x / BDOF_MIN_BLOCK_SIZE][4] = save[4];
+                }
+ */
+
+static void vvc_derive_bdof_vx_vy_8x(const int16_t *_src0, const int16_t *_src1, int16_t *gradient_h[2], int16_t *gradient_v[2], int16_t vx[16], int16_t vy[16], int block_h)
+{
+    int line_table[2][4][5] = {0};
+    const int thres = 1 << 4;
+    for (int y = 0; y < block_h; y += BDOF_MIN_BLOCK_SIZE) {
+        int sgx2[2] = {0};
+        int sgy2[2] = {0};
+        int sgxgy[2] = {0};
+        int sgxdi[2] = {0};
+        int sgydi[2] = {0};
+
+        {
+            const int16_t *src0 = _src0 + y * MAX_PB_SIZE;
+            const int16_t *src1 = _src1 + y * MAX_PB_SIZE;
+            int idx = BDOF_BLOCK_SIZE * y;
+            const int16_t *gh[] = {gradient_h[0] + idx, gradient_h[1] + idx};
+            const int16_t *gv[] = {gradient_v[0] + idx, gradient_v[1] + idx};
+            const int shift2 = 4;
+            const int shift3 = 1;
+            const int padLeft = 1;
+            const int padTop = !y;
+            const int padRight = 0;
+            const int padBottom = y + BDOF_MIN_BLOCK_SIZE == block_h;
+
+            for (int y2 = -1; y2 < BDOF_MIN_BLOCK_SIZE + 1; y2++) {
                 const int dy = y2 + (padTop && y2 < 0) - (padBottom && y2 == BDOF_MIN_BLOCK_SIZE);
                 const int16_t *src02 = src0 + dy * MAX_PB_SIZE;
                 const int16_t *src12 = src1 + dy * MAX_PB_SIZE;
@@ -114,29 +134,59 @@ static void vvc_derive_bdof_vx_vy_8x(const int16_t *_src0, const int16_t *_src1,
                     save[3] += -VVC_SIGN(temph) * diff;
                     save[4] += -VVC_SIGN(tempv) * diff;
                 }
-                sgx2 += save[0];
-                sgy2 += save[1];
-                sgxgy += save[2];
-                sgxdi += save[3];
-                sgydi += save[4];
-
-                if (y2 == BDOF_MIN_BLOCK_SIZE - 1 || (y == 0 && y2 == -1)) {
-                    line_table[0][x / BDOF_MIN_BLOCK_SIZE][0] = save[0];
-                    line_table[0][x / BDOF_MIN_BLOCK_SIZE][1] = save[1];
-                    line_table[0][x / BDOF_MIN_BLOCK_SIZE][2] = save[2];
-                    line_table[0][x / BDOF_MIN_BLOCK_SIZE][3] = save[3];
-                    line_table[0][x / BDOF_MIN_BLOCK_SIZE][4] = save[4];
-                } else if (y2 == BDOF_MIN_BLOCK_SIZE) {
-                    line_table[1][x / BDOF_MIN_BLOCK_SIZE][0] = save[0];
-                    line_table[1][x / BDOF_MIN_BLOCK_SIZE][1] = save[1];
-                    line_table[1][x / BDOF_MIN_BLOCK_SIZE][2] = save[2];
-                    line_table[1][x / BDOF_MIN_BLOCK_SIZE][3] = save[3];
-                    line_table[1][x / BDOF_MIN_BLOCK_SIZE][4] = save[4];
-                }
+                sgx2[0] += save[0];
+                sgy2[0] += save[1];
+                sgxgy[0] += save[2];
+                sgxdi[0] += save[3];
+                sgydi[0] += save[4];
             }
-            vx[y + x / BDOF_MIN_BLOCK_SIZE] = sgx2 > 0 ? av_clip((sgxdi * (1 << 2)) >> av_log2(sgx2), -thres + 1, thres - 1) : 0;
-            vy[y + x / BDOF_MIN_BLOCK_SIZE] = sgy2 > 0 ? av_clip( ((sgydi * (1 << 2)) - ((vx[y + x / BDOF_MIN_BLOCK_SIZE] * sgxgy) >> 1)) >> av_log2(sgy2), -thres + 1, thres - 1) : 0;
         }
+
+        {
+            const int16_t *src0 = _src0 + y * MAX_PB_SIZE + BDOF_MIN_BLOCK_SIZE;
+            const int16_t *src1 = _src1 + y * MAX_PB_SIZE + BDOF_MIN_BLOCK_SIZE;
+            int idx = BDOF_BLOCK_SIZE * y + BDOF_MIN_BLOCK_SIZE;
+            const int16_t *gh[] = {gradient_h[0] + idx, gradient_h[1] + idx};
+            const int16_t *gv[] = {gradient_v[0] + idx, gradient_v[1] + idx};
+            const int shift2 = 4;
+            const int shift3 = 1;
+            const int padLeft = 0;
+            const int padTop = !y;
+            const int padRight = 1;
+            const int padBottom = y + BDOF_MIN_BLOCK_SIZE == block_h;
+
+            for (int y2 = -1; y2 < BDOF_MIN_BLOCK_SIZE + 1; y2++) {
+                const int dy = y2 + (padTop && y2 < 0) - (padBottom && y2 == BDOF_MIN_BLOCK_SIZE);
+                const int16_t *src02 = src0 + dy * MAX_PB_SIZE;
+                const int16_t *src12 = src1 + dy * MAX_PB_SIZE;
+
+                int save[5] = {0};
+                for (int x1 = -1; x1 < BDOF_MIN_BLOCK_SIZE + 1; x1++) {
+                    const int dx = x1 + (padLeft && x1 < 0) - (padRight && x1 == BDOF_MIN_BLOCK_SIZE);
+                    const int diff = (src02[dx] >> shift2) - (src12[dx] >> shift2);
+                    const int idx1 = BDOF_BLOCK_SIZE * dy + dx;
+                    const int temph = (gh[0][idx1] + gh[1][idx1]) >> shift3;
+                    const int tempv = (gv[0][idx1] + gv[1][idx1]) >> shift3;
+
+                    save[0] += FFABS(temph);
+                    save[1] += FFABS(tempv);
+                    save[2] += VVC_SIGN(tempv) * temph;
+                    save[3] += -VVC_SIGN(temph) * diff;
+                    save[4] += -VVC_SIGN(tempv) * diff;
+                }
+                sgx2[1] += save[0];
+                sgy2[1] += save[1];
+                sgxgy[1] += save[2];
+                sgxdi[1] += save[3];
+                sgydi[1] += save[4];
+            }
+        }
+
+        vx[y] = sgx2[0] > 0 ? av_clip((sgxdi[0] * (1 << 2)) >> av_log2(sgx2[0]), -thres + 1, thres - 1) : 0;
+        vy[y] = sgy2[0] > 0 ? av_clip( ((sgydi[0] * (1 << 2)) - ((vx[y] * sgxgy[0]) >> 1)) >> av_log2(sgy2[0]), -thres + 1, thres - 1) : 0;
+
+        vx[y + 1] = sgx2[1] > 0 ? av_clip((sgxdi[1] * (1 << 2)) >> av_log2(sgx2[1]), -thres + 1, thres - 1) : 0;
+        vy[y + 1] = sgy2[1] > 0 ? av_clip( ((sgydi[1] * (1 << 2)) - ((vx[y + 1] * sgxgy[1]) >> 1)) >> av_log2(sgy2[1]), -thres + 1, thres - 1) : 0;
     }
 }
 
@@ -161,44 +211,6 @@ static void vvc_derive_bdof_vx_vy_16x(const int16_t *_src0, const int16_t *_src1
             const int padBottom = y + BDOF_MIN_BLOCK_SIZE == block_h;
 
             for (int y2 = -1; y2 < BDOF_MIN_BLOCK_SIZE + 1; y2++) {
-                if (y == 0) {
-                    // pad top, copy last line
-                    if (y2 == 0) {
-                        sgx2 += line_table[0][x / BDOF_MIN_BLOCK_SIZE][0];
-                        sgy2 += line_table[0][x / BDOF_MIN_BLOCK_SIZE][1];
-                        sgxgy += line_table[0][x / BDOF_MIN_BLOCK_SIZE][2];
-                        sgxdi += line_table[0][x / BDOF_MIN_BLOCK_SIZE][3];
-                        sgydi += line_table[0][x / BDOF_MIN_BLOCK_SIZE][4];
-                        continue;
-                    }
-                } else if (y > 0) {
-                    if (y2 == -1) {
-                        sgx2 += line_table[0][x / BDOF_MIN_BLOCK_SIZE][0];
-                        sgy2 += line_table[0][x / BDOF_MIN_BLOCK_SIZE][1];
-                        sgxgy += line_table[0][x / BDOF_MIN_BLOCK_SIZE][2];
-                        sgxdi += line_table[0][x / BDOF_MIN_BLOCK_SIZE][3];
-                        sgydi += line_table[0][x / BDOF_MIN_BLOCK_SIZE][4];
-                        continue;
-                    } else if (y2 == 0) {
-                        sgx2 += line_table[1][x / BDOF_MIN_BLOCK_SIZE][0];
-                        sgy2 += line_table[1][x / BDOF_MIN_BLOCK_SIZE][1];
-                        sgxgy += line_table[1][x / BDOF_MIN_BLOCK_SIZE][2];
-                        sgxdi += line_table[1][x / BDOF_MIN_BLOCK_SIZE][3];
-                        sgydi += line_table[1][x / BDOF_MIN_BLOCK_SIZE][4];
-                        continue;
-                    }
-
-                    // pad bottom, copy last line
-                    if (y + BDOF_MIN_BLOCK_SIZE == block_h && y2 == BDOF_MIN_BLOCK_SIZE) {
-                        sgx2 += line_table[0][x / BDOF_MIN_BLOCK_SIZE][0];
-                        sgy2 += line_table[0][x / BDOF_MIN_BLOCK_SIZE][1];
-                        sgxgy += line_table[0][x / BDOF_MIN_BLOCK_SIZE][2];
-                        sgxdi += line_table[0][x / BDOF_MIN_BLOCK_SIZE][3];
-                        sgydi += line_table[0][x / BDOF_MIN_BLOCK_SIZE][4];
-                        break;
-                    }
-                }
-
                 const int dy = y2 + (padTop && y2 < 0) - (padBottom && y2 == BDOF_MIN_BLOCK_SIZE);
                 const int16_t *src02 = src0 + dy * MAX_PB_SIZE;
                 const int16_t *src12 = src1 + dy * MAX_PB_SIZE;
@@ -222,20 +234,6 @@ static void vvc_derive_bdof_vx_vy_16x(const int16_t *_src0, const int16_t *_src1
                 sgxgy += save[2];
                 sgxdi += save[3];
                 sgydi += save[4];
-
-                if (y2 == BDOF_MIN_BLOCK_SIZE - 1 || (y == 0 && y2 == -1)) {
-                    line_table[0][x / BDOF_MIN_BLOCK_SIZE][0] = save[0];
-                    line_table[0][x / BDOF_MIN_BLOCK_SIZE][1] = save[1];
-                    line_table[0][x / BDOF_MIN_BLOCK_SIZE][2] = save[2];
-                    line_table[0][x / BDOF_MIN_BLOCK_SIZE][3] = save[3];
-                    line_table[0][x / BDOF_MIN_BLOCK_SIZE][4] = save[4];
-                } else if (y2 == BDOF_MIN_BLOCK_SIZE) {
-                    line_table[1][x / BDOF_MIN_BLOCK_SIZE][0] = save[0];
-                    line_table[1][x / BDOF_MIN_BLOCK_SIZE][1] = save[1];
-                    line_table[1][x / BDOF_MIN_BLOCK_SIZE][2] = save[2];
-                    line_table[1][x / BDOF_MIN_BLOCK_SIZE][3] = save[3];
-                    line_table[1][x / BDOF_MIN_BLOCK_SIZE][4] = save[4];
-                }
             }
             vx[y + x / BDOF_MIN_BLOCK_SIZE] = sgx2 > 0 ? av_clip((sgxdi * (1 << 2)) >> av_log2(sgx2), -thres + 1, thres - 1) : 0;
             vy[y + x / BDOF_MIN_BLOCK_SIZE] = sgy2 > 0 ? av_clip( ((sgydi * (1 << 2)) - ((vx[y + x / BDOF_MIN_BLOCK_SIZE] * sgxgy) >> 1)) >> av_log2(sgy2), -thres + 1, thres - 1) : 0;
